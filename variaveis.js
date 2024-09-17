034e4b802280f967b6d5c487adcf9e41bcc7f721
Felipe Cardoso Pacheco <160873082+felipecardosopacheco@users.noreply.github.com>
2024-09-17 12:10:02 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,10 @@
+//tipo number
+
+// Var era utilizado para todos as variáveis
+var numero01 = 3
+// let é uma atualização do var e é passivel de variação
+let numero02 = 10
+// const é uma maneira de declarar uma variável que irá permanecer constante
+const numero03 = 7
+
+const soma = numero01 + numero03
