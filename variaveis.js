c545abbbf652326202752fe7e526295aca556416
Felipe Cardoso Pacheco <160873082+felipecardosopacheco@users.noreply.github.com>
2024-09-18 07:55:26 -0300

    Add files via upload

--- variaveis.js
+++ variaveis.js
@@ -8,3 +8,57 @@ let numero02 = 10
 const numero03 = 7
 
 const soma = numero01 + numero03
+const subtracao = numero03 * numero01
+const multiplicacao = numero03 * numero02
+const divisao = numero02 / numero01
+
+console.log (soma)
+console.log(subtracao)
+console.log (multiplicacao)
+console.log (divisao)
+
+
+// Tipo Float -- ponto flutuante
+
+const numero04 = 3.2
+const numero05 = .5
+
+const multiplicacao2 = numero04 + numero05
+
+console.log(multiplicacao2)
+
+//NaN -> Not a Number
+
+const nome = "Felipe"
+const falsonumero = '7'
+
+const naoEnumero = nome * 0.4
+const concatenacao01 = falsonumero + numero04
+
+console.log (naoEnumero)
+console.log(concatenacao01)
+
+// Dados do tipo String = Textos
+
+const frase = 'Meu nome é'
+const frase2 = 'O segundo DS é  "estudioso", não é?' 
+
+const concatenacao02 = frase + nome
+
+console.log(concatenacao02)
+
+//Dados do tipo Boolean
+//True -> verdadeiro
+//False -> Falso
+
+const numero06 = 7
+
+const comparacao01 = numero06 == falsonumero
+const comparacao02 = numero06 === falsonumero
+
+//Um sinal de igual (=) é para declarar
+// Dois sinais de igual (==) comparando os :valores e não o tipo do dado (comparação fraca)
+// Três sinais de igual(===) comparando os tipos do dado.(comparação forte)
+
+console.log(comparacao01)
+console.log(comparacao02)
